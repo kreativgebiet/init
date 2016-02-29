da3b3e58518239b4e5ee1316b75e659258955aac
Dominik Schmidt <visualcake@me.com>
2016-02-29 12:08:57 +0100

    add help section to cli

--- bin/init.js
+++ bin/init.js
@@ -3,11 +3,20 @@
 
 require('babel-register');
 var argv = require('yargs')
-  .default({
-    git: true,
-    init: true,
-  }).argv;
+  .usage('Usage: $0 <repository> [options]')
+  .example('$0 kreativgebiet/init-example --no-git')
+  .demand(1)
+  .alias('b', 'bare')
+  .default({ git: false, init: false, bare: false })
+  .help('h')
+  .alias('h', 'help')
+  .describe('bare', 'Initialize a bare git repository')
+  .describe('no-git', 'Skip everything git related in finish task')
+  .describe('no-init', 'Skip the first git commit, but create a local repository')
+  .describe('git', 'Skip the first git commit, but create a local repository')
+  .epilog('Copyright (c) 2016 by Kreativgebiet')
+  .argv;
 
 require('../lib')({
-  args: argv
+  opts: argv
 });
